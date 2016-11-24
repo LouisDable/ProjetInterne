bb09669328e5276bee53d5e5c8b0f32e03337acd
LouisDable <Dablelouisqc@gmail.com>
2016-11-24 15:35:07 +0100

    Creation de la branche pharell

--- DemoMongo/src/main/java/com/DemoMongo/DemoMongo.java
+++ DemoMongo/src/main/java/com/DemoMongo/DemoMongo.java
@@ -6,7 +6,7 @@ public class DemoMongo {
 	private static String nameDb = "db_PI";
 	
 	public static void main(String[] args) {
-		System.out.println("Hello team");
+		System.out.println("Hello team from pharell");
 	}
 
 }
